c29a2bbb65375be7200fd459b4cd76f38f341c42
malsinim <malsini.masachchige@dcmail.ca>
2024-02-11 20:41:26 -0500

    Initial Commit with complete changes to index.html

---
+++ lab-1/scripts/app.js
@@ -0,0 +1,6 @@
+
+// Injecting text content for each page
+let homeContent = "Hi! welcome to my website :D My name is Malsini and this is a website I created \nas a course requirement for "+
+"INFT2202 - Web Development CSS!\n\n I had tons of fun creating this website and I hope you will like it!\n Thank you for visiting my site"+
+"\n\nFeel free to check all the pages Ive included in the nav bar :D";
+document.getElementById("home-contents").innerText = homeContent;
