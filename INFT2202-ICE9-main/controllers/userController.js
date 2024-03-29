3206d877861d680b4f514b1a872d3054f3f38704
malsinim <malsini.masachchige@dcmail.ca>
2024-03-29 14:30:27 -0400

    Initial Commit

---
+++ INFT2202-ICE9-main/controllers/userController.js
@@ -0,0 +1,20 @@
+
+exports.homeView = (req, res) => {
+  res.render('home');
+};
+
+exports.aboutView = (req, res) => {
+  res.render('about');
+};
+
+exports.pricingView = (req, res) => {
+  res.render('pricing');
+};
+
+exports.servicesView = (req, res) => {
+  res.render('services');
+};
+
+exports.productsView = (req, res) => {
+  res.render('products');
+};
---
+++ INFT2202-ICE9-main/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/userController');
+
+router.get('/', usersController.homeView);
+router.get('/about', usersController.aboutView); 
+router.get('/pricing', usersController.pricingView);
+router.get('/services', usersController.servicesView); 
+router.get('/products', usersController.productsView);
+
+module.exports = router;
+
